3c01b87136dcc9318d86ef4607bd1b729ba21419
Ghom <camille.abella@hotmail.fr>
2019-08-14 13:08:16 +0200

    preview - first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const ReactionRole = require("./ReactionRole.js")
+const ReactionRoleMessage = require("./ReactionRoleMessage.js")
+
+class Rero {
+    // Gestion de tout le bordel
+    // Listeners et compagnie
+}
+
+module.exports = Rero
